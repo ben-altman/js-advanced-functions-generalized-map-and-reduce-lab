a293fef34f1f00016a9a76cf998977268e13b7c4
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-18 21:06:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(source, workToDo) {
+    const ritorno = [];
+    for (let x of source) {
+        ritorno.push(workToDo(x));
+    };
+    return ritorno;
+};
+
+function reduce(source, workToDo, start) {
+    let total;
+    if (start) {
+        total = start;
+        for (let i = 0; i < source.length; i++) {
+            total = workToDo(source[i], total);
+        }
+    } else {
+        total = source[0];
+        for (let i = 0; i < source.length; i++) {
+            total = workToDo(source[i], total);
+        }
+    }
+    return total;
+};
